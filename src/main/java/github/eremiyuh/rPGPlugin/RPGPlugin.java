fc7efb5492f10fea266fbccb56ab18ad5c6134bf
eremiyuh <jejomic@gmail.com>
2024-10-20 22:33:02 +0800

    attributes , and swordies str attribute update

--- src/main/java/github/eremiyuh/rPGPlugin/RPGPlugin.java
+++ src/main/java/github/eremiyuh/rPGPlugin/RPGPlugin.java
@@ -1,36 +1,55 @@
 package github.eremiyuh.rPGPlugin;
 
-import github.eremiyuh.rPGPlugin.commands.CheckClassCommand;
-import github.eremiyuh.rPGPlugin.commands.SelectClassCommand;
-import github.eremiyuh.rPGPlugin.listeners.PlayerClassListener;
-import github.eremiyuh.rPGPlugin.manager.PlayerClassManager;
+import github.eremiyuh.rPGPlugin.commands.ConvertLevelsCommand;
+import github.eremiyuh.rPGPlugin.commands.SelectElement;
+import github.eremiyuh.rPGPlugin.commands.SelectRace;
+import github.eremiyuh.rPGPlugin.commandswithgui.CheckClassCommand;
+import github.eremiyuh.rPGPlugin.commandswithgui.SelectClassCommand;
+import github.eremiyuh.rPGPlugin.listeners.OptimizedVampireSunlightListener;
+import github.eremiyuh.rPGPlugin.listeners.PlayerJoinListener;
+import github.eremiyuh.rPGPlugin.listeners.PlayerQuitListener;
+import github.eremiyuh.rPGPlugin.manager.PlayerProfileManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class RPGPlugin extends JavaPlugin {
+import java.util.Objects;
 
-    private SelectClassCommand selectClassCommand;
-    private CheckClassCommand checkClassCommand;
-    private PlayerClassManager playerClassManager;
+public class RPGPlugin extends JavaPlugin {
+
+    private PlayerProfileManager profileManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        playerClassManager = new PlayerClassManager(this); // Initialize PlayerClassManager
+        // Initialize the profile manager
+        profileManager = new PlayerProfileManager(this);
 
-        // Register commands
-        selectClassCommand = new SelectClassCommand(playerClassManager);
-        checkClassCommand = new CheckClassCommand(playerClassManager);
-        getCommand("selectclass").setExecutor(selectClassCommand);
-        getCommand("checkclass").setExecutor(checkClassCommand);
+        // Register events
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(profileManager), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(profileManager), this);
+        Objects.requireNonNull(getCommand("selectclass")).setExecutor(new SelectClassCommand(this, profileManager));
+        getServer().getPluginManager().registerEvents(new OptimizedVampireSunlightListener(profileManager, this), this);
 
-        // Register listener
-        PlayerClassListener playerClassListener = new PlayerClassListener(playerClassManager);
-        getServer().getPluginManager().registerEvents(playerClassListener, this);
-        getServer().getPluginManager().registerEvents(checkClassCommand, this);
+        // Register the command executor
+        Objects.requireNonNull(this.getCommand("checkclass")).setExecutor(new CheckClassCommand(profileManager));
+        Objects.requireNonNull(getCommand("convertlevels")).setExecutor(new ConvertLevelsCommand(profileManager));
+        Objects.requireNonNull(getCommand("selectelement")).setExecutor(new SelectElement(profileManager));
+        Objects.requireNonNull(getCommand("selectrace")).setExecutor(new SelectRace(profileManager));
+        // Register the listener
+        Bukkit.getPluginManager().registerEvents(new CheckClassCommand(profileManager), this);
+
+
+        // Log to console that the plugin has been enabled
+        getLogger().info("RPGPlugin has been enabled.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Save all profiles when the server is shutting down
+        profileManager.saveAllProfiles();
+        getLogger().info("All player profiles have been saved on server shutdown.");
+
+        // Log to console that the plugin has been disabled
+        getLogger().info("RPGPlugin has been disabled.");
     }
+
 }
