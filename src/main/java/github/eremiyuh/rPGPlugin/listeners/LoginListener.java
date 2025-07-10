810b7412e32b60e1c4116c72f2f054b97327ca8a
jejom <jejomic@gmail.com>
2025-07-10 21:15:38 +0800

    added filter chat feature, incomplete

--- src/main/java/github/eremiyuh/rPGPlugin/listeners/LoginListener.java
+++ src/main/java/github/eremiyuh/rPGPlugin/listeners/LoginListener.java
@@ -3,9 +3,12 @@ package github.eremiyuh.rPGPlugin.listeners;
 import github.eremiyuh.rPGPlugin.RPGPlugin;
 import github.eremiyuh.rPGPlugin.manager.PlayerProfileManager;
 import github.eremiyuh.rPGPlugin.profile.UserProfile;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
@@ -32,4 +35,40 @@ public class LoginListener implements Listener {
                     "Server is still starting up, please try again in a moment.");
         }
     }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        String message = event.getMessage().toLowerCase();
+
+        if (containsBadWord(message)) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.RED + "⚠ Your message was blocked due to inappropriate language.");
+
+            // Optional: log to console or take further action
+            Bukkit.getLogger().warning("[ChatFilter] " + event.getPlayer().getName() + " tried to say: " + message);
+
+            // Optional: kick or ban
+            // event.getPlayer().kickPlayer("You were kicked for using offensive language.");
+            // Bukkit.getBanList(BanList.Type.NAME).addBan(event.getPlayer().getName(), "Banned for profanity", null, null);
+        }
+    }
+
+    private boolean containsBadWord(String message) {
+        String[] bannedWords = {
+                // Tagalog
+                "bobo", "tanga", "gago", "ulol", "puta", "pakyu", "tang i","tangina","siraulo", "sira ulo","puki","puke",
+                "tits","titi","tite","burat","bembang","kantot","kantut","pota","bubu",
+                // English
+                "fuck", "shit", "asshole", "idiot", "bastard","ass","nigga","fuk","dick","pussy"
+        };
+
+        for (String word : bannedWords) {
+            if (message.contains(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
